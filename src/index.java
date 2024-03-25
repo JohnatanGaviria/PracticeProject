db36fc6c29ff4691194bf3e58883749ff49d5ae0
sayancookie <mr_cookie01@yahoo.com>
2024-03-25 17:29:26 -0400

    'try'

---
+++ src/index.java
@@ -0,0 +1,15 @@
+
+public class index {
+
+	public static void main(String[] args) {
+		
+		String greetings = "hello";
+		
+		System.out.println(greetings);
+		
+		for (int i= 0; i < 5; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
