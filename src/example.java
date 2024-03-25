596cdd82cc84f9b1aef1ae572fc8b51ee275edb9
sayancookie <mr_cookie01@yahoo.com>
2024-03-25 17:54:37 -0400

    'inverse'

---
+++ src/example.java
@@ -0,0 +1,16 @@
+
+public class example {
+
+	public static void main(String[] args) {
+		
+		String goodby = "see you later";
+		
+		System.out.println(goodby);
+		
+		for (int i= 5; i >= 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
