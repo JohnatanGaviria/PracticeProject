cff504b32405ea34cc8ddfe84b837874df1363e1
sayancookie <mr_cookie01@yahoo.com>
2024-03-30 19:47:20 -0400

    'new-variables'

--- src/index.java
+++ src/index.java
@@ -4,12 +4,29 @@ public class index {
 	public static void main(String[] args) {
 		
 		String greetings = "hello";
+		String convoStarter = "how are you?";
+//		String fancyWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
+
+		
+//		int ageYears = 5;
+//		float ageMonths = 8.5f;
 		
 		System.out.println(greetings);
+		System.out.println(convoStarter);
+		smartyPants("pneumonoultramicroscopicsilicovolcanoconiosis");
+		smartyPants("mississippi");
+//		System.out.println(greetings.length());
+//		System.out.println(fancyWord.length());
 		
-		for (int i= 0; i < 5; i++) {
-			System.out.println(i);
-		}
+//		for (int i= 0; i < 5; i++) {
+//			System.out.println(i);
+//		}
+	}
+	private static void smartyPants(String fancyWord) {
+//		String fancyWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
+		System.out.println("theres a word that reads");
+		System.out.println(fancyWord);
+		System.out.println("it is " + fancyWord.length() + " letters long");
 	}
 
 }
